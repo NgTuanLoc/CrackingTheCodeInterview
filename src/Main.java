72de27044977d56c09112ad09bb7e2af39bb7ec9
NgTuanLoc <tuanloc2352000@gmail.com>
2023-01-14 08:21:22 +0700

    First Setup

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+   public static void main(String[] args) {
+	  Student Loc = new Student("Loc");
+	  Loc.set_name("LocLoc");
+	  System.out.println(Loc.get_name());
+   }
+}
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Student {
+   private String _name;
+
+   Student(String name) {
+	  _name = name;
+   }
+
+   public void set_name(String name) {
+	  this._name = name;
+   }
+
+   public String get_name() {
+	  return _name;
+   }
+}
